2640dc3d2f98e76d255c054f8647bfdd96d67336
Programmer 8PR <74245727+t8pr@users.noreply.github.com>
2022-03-28 17:58:21 +0300

    Update script.js

--- Qr Code/js/script.js
+++ Qr Code/js/script.js
@@ -1,13 +1,23 @@
 const search = document.getElementById('search-bar');
 const btn = document.getElementById('btn');
 const down = document.getElementById('Download');
-const qrimg = document.getElementById('qrcode')
-
+const qrimg = document.getElementById('qrcode');
+const card = document.querySelector(".card");
+const emptyh2 = document.querySelector(".empty h2");
+const link = document.createElement('a')
 btn.addEventListener('click', gen);
-
 function gen() {
-    const qr = search.value
-    const qrcode = `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${qr}`
-    qrimg.src = qrcode
-    down.href = `${qrcode}`
+    const qr = search.value;
+    const qrcode = `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${qr}`;
+    card.style.height = "35rem";
+    if (qrcode == "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data="){
+        emptyh2.style.display = "inline-block";
+        down.style.display = "none";
+    }
+    else {
+        emptyh2.style.display = "none";
+        qrimg.src = qrcode;
+        down.style.display = "inline-block";
+        down.href = qrcode;
+    }
 }
